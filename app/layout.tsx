9cb156fea814a40d39fa85e3ed5cd3cc7370569e
rowdyGit13 <127695758+rowdyGit13@users.noreply.github.com>
2025-02-27 17:04:17 -0600

    auth 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Providers } from "@/components/utilities/providers";
 import { Toaster } from "@/components/ui/sonner";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
-
-
-const inter = Inter({
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
